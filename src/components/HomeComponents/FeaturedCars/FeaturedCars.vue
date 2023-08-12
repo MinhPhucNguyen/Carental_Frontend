f93eb15f78822d658738d05bd86036e61065a615
MinhPhucNguyen <99171076+MinhPhucNguyen@users.noreply.github.com>
2023-08-12 23:55:15 +0700

    Up code

--- src/components/HomeComponents/FeaturedCars/FeaturedCars.vue
+++ src/components/HomeComponents/FeaturedCars/FeaturedCars.vue
@@ -11,18 +11,42 @@
         <car-item />
         <car-item />
         <car-item />
+        {{ randomCars }}
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import { ref } from "vue";
 import CarItem from "../CarItemCard/CarItem.vue";
 
 export default {
   name: "FeaturedCarsSection",
   components: {
     CarItem,
+  },
+  setup() {
+    const randomCars = ref([]);
+
+    const getRandomCars = async () => {
+      try {
+        const response = await axios.get(
+          "http://127.0.0.1:8000/api/v2/cars/randomCars"
+        );
+
+        console.log(response.data);
+        randomCars.value = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getRandomCars();
+
+    return {
+      randomCars,
+    };
   },
 };
 </script>
